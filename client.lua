c7bc3ec6c59a341284452558ff86bfb178e21233
WilliamDASILVA <william.da.silva@outlook.com>
2013-08-31 23:06:39 +0200

    Main commit

---
+++ client.lua
@@ -0,0 +1,34 @@
+﻿
+
+txd = engineLoadTXD ( "models/roads_texture.txd" )
+engineImportTXD ( txd, 2670 )
+engineImportTXD ( txd, 2671 )
+engineImportTXD ( txd, 2672 )
+engineImportTXD ( txd, 2673 )
+engineImportTXD ( txd, 2674 )
+
+col = engineLoadCOL ( "models/corner_long.col" )
+dff = engineLoadDFF ( "models/corner_long.dff", 2670 )
+engineReplaceCOL (col, 2670)
+engineReplaceModel (dff, 2670)
+col = engineLoadCOL ( "models/corner_short.col" )
+dff = engineLoadDFF ( "models/corner_short.dff", 2671 )
+engineReplaceCOL (col, 2671)
+engineReplaceModel (dff, 2671)
+col = engineLoadCOL ( "models/cross_junction.col" )
+dff = engineLoadDFF ( "models/cross_junction.dff", 2672 )
+engineReplaceCOL (col, 2672)
+engineReplaceModel (dff, 2672)
+col = engineLoadCOL ( "models/straight_road.col" )
+dff = engineLoadDFF ( "models/straight_road.dff", 2673 )
+engineReplaceCOL (col, 2673)
+engineReplaceModel (dff, 2673)
+col = engineLoadCOL ( "models/t_junction.col" )
+dff = engineLoadDFF ( "models/t_junction.dff", 2674 )
+engineReplaceCOL (col, 2674)
+engineReplaceModel (dff, 2674)
+
+
+
+
+--[[ Events ]]--
---
+++ server.lua
@@ -0,0 +1,139 @@
+﻿
+
+-- Generate world
+addCommandHandler("pos",
+function(player, cmd)
+	local x, y, z = getElementPosition(player)
+	outputChatBox(x..", "..y..", "..z)
+end)
+
+
+World = {
+	objects = {}
+}
+
+
+
+function World.generate(dimension)
+	if dimension then
+		for k = 0, dimension do
+			if k%2 == 0 then -- Si la ligne est une ligne est pair
+				for i = 0, dimension do
+					if i == 0 then
+						-- createObject(2671, -15,13.5, 50, 0, 0, 180)
+						if k == 0 then -- Si le coin, on met un qui tourne
+							createObject(2671,
+													i*(-15)-15,
+													(13.5*k)+13.5,
+													50,
+													0,0,180
+													)
+
+						else -- Si ce n'es pas un coin, on met un T
+							createObject(2674,
+													(( i*15)-15)-15/2,
+													(15*k)+6,
+													50,
+													0,0,90
+													)
+						end
+					-- elseif i == dimension then
+						-- outputDebugString("Last"..i)
+					else
+					
+						if i%2 == 0 then
+							if k == 0 then -- Si c'est le premier de la ligne, on met le T
+								createObject(2674,
+													(( i*15)-15)-15/2,
+													6,
+													50,
+													0,0,180
+													)
+
+							else -- Sinon, c'est que c'est un dans le milieu alors on met on +
+								createObject(2672,
+													(( i*15)-15)-15/2,
+													((k*15)+6),
+													50,
+													0,0,180
+													)
+							end
+							
+							if i == dimension-1 then -- Creation du dernier T de la ligne
+								if k == 0 then
+								
+								else
+								
+									createObject(2674,
+														(i*15)+7.5,
+														(k*15)+6,
+														50,
+														0,0,270
+														)
+								end
+							end
+						else
+							-- routes verticale
+							createObject(2673,
+												(i*15)-15,
+												(k*15),
+												50,
+												0, 0, 90
+												)
+												
+						end
+						
+						if k == 0 then -- Si dans la première ligne et dans la dernière colone, on met un qui tourne
+							if i == dimension then
+								createObject(2671,
+														(i*15)-15,
+														13.5,
+														50,
+														0,0,270
+														)
+							end
+						end
+					end
+				end
+			else -- Sinon, la ligne est impair
+				for i = 0, dimension do
+					if i%2 == 0 then -- Si c'est pair
+						if i == 0 then
+							createObject(2673,
+													0+((i*15)+1.5),
+													(k*15)-1.5,
+													50,
+													0, 0, 0
+													)
+							-- On cr�e aussi celui qui est au point 0
+							createObject(2673,
+													(-15)+(-13.5),
+													(k*15)-1.5,
+													50,
+													0, 0, 0
+													)
+						else
+							createObject(2673,
+													0+((i*15)+1.5),
+													(k*15)-1.5,
+													50,
+													0, 0, 0
+													)
+						end
+					else -- Impair, on met rien
+						
+					end
+				end
+			end
+		end
+	end
+end
+
+function World.resStart()
+	World.generate(9)
+end
+
+
+
+--[[ Events ]]--
+addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), World.resStart)
